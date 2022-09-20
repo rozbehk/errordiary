7c5b5c1ac1fa67f63f8125a2d933f27389d26d64
deegyi <dee_gyi@live.ca>
2022-09-20 16:57:44 -0400

    yes

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class Admins(admin.ModelAdmin):
-    list_display = ('title', 'user')
+# class Admins(admin.ModelAdmin):
+#     list_display = ('title', 'user')
 
-admin.site.register(Error, Admins)
+admin.site.register(Error)
 admin.site.register(Comment)
 admin.site.register(Screenshot)
