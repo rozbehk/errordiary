9d6cdd5792dbfe1779270ee49489b0f31568e439
deegyi <dee_gyi@live.ca>
2022-09-20 17:18:48 -0400

    update

--- errordiary/urls.py
+++ errordiary/urls.py
@@ -18,10 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('', include('main_app.urls'))
-    
-=======
     path('', include('main_app.urls')),
->>>>>>> c4686a4ce03f0466569fcbbdaead9cdd6ef08af7
 ]
